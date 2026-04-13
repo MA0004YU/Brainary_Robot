db683fe79dbec41a9ebb80cfb8f4c903a301c544
Fl-sk-y <157768213+Fl-sk-y@users.noreply.github.com>
2026-04-13 12:09:52 +0800

    Delete embodiedbench/management directory

--- embodiedbench/management/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Management layer: closed-loop control aligned with repair/brain_api_example; pluggable robot body."""
-
-from .planning_loop import TaskPlanningLoop
-from .heuristic_policy import BrightnessAvoidancePolicy
-
-# Import RepairWalkBody / SapienVelocityBody from submodules to avoid heavy deps at package import.
-
-__all__ = ["TaskPlanningLoop", "BrightnessAvoidancePolicy"]
